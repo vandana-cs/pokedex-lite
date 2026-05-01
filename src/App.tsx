8b211074e0fb7086ed1e2dcdc007c142bce1feb0
rakesh <csrakesh32@gmail.com>
2026-05-01 18:37:08 +0530

    fix: removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [pokemon, setPokemon] = useState<any[]>([]);
+  
   const [detailedPokemon, setDetailedPokemon] = useState<any[]>([]);
   const [loading, setLoading] = useState(false);
 
